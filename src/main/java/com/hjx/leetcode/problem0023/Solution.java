7b58f73198c68a428212e9f13cc5e838c9c6032d
houjinxiang <houjinxiang@fangdd.com>
2019-03-29 19:44:46 +0800

    https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/main/java/com/hjx/leetcode/problem0023/Solution.java
@@ -0,0 +1,9 @@
+package com.hjx.leetcode.problem0023;
+
+import com.hjx.leetcode.entity.ListNode;
+
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return null;
+    }
+}
