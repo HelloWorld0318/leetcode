93f000327c017dfac4c1969ebde730dd6cb9b163
houjinxiang <houjinxiang@fangdd.com>
2019-05-18 12:28:41 +0800

    https://leetcode.com/problems/subsets-ii/

--- src/main/java/com/hjx/leetcode/problem0090/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0090/Solution.java
@@ -10,19 +10,19 @@ public class Solution {
         if (nums.length != 0) {
             Arrays.sort(nums);
             // 广度优先遍历
-            bfs(nums, 0, new ArrayList<>(), ans);
+            dfs(nums, 0, new ArrayList<>(), ans);
         }
         return ans;
     }
 
-    private void bfs(int[] nums, int index, List<Integer> item, List<List<Integer>> ans) {
+    private void dfs(int[] nums, int index, List<Integer> item, List<List<Integer>> ans) {
         ans.add(new ArrayList<>(item));
         for (int i = index; i < nums.length; i++) {
             if (i > index && nums[i] == nums[i - 1]) {
                 continue;
             }
             item.add(nums[i]);
-            bfs(nums, i + 1, item, ans);
+            dfs(nums, i + 1, item, ans);
             item.remove(item.size() - 1);
         }
     }
