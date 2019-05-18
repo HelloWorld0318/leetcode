cfd145a678b2faf09d8d332b9057976e8e86105a
houjinxiang <houjinxiang@fangdd.com>
2019-05-18 17:38:46 +0800

    https://leetcode.com/problems/generate-parentheses/

--- src/main/java/com/hjx/leetcode/problem0022/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0022/Solution.java
@@ -1,10 +1,27 @@
 package com.hjx.leetcode.problem0022;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<String> generateParenthesis(int n) {
-        // TODO problem0022
-        return null;
+        List<String> ans = new ArrayList<>();
+        if (n != 0) {
+            dfs(n, 0, 0, "", ans);
+        }
+        return ans;
+    }
+
+    private void dfs(int n, int left, int right, String item, List<String> ans) {
+        if (left == n && right == n) {
+            ans.add(item);
+            return;
+        }
+        if (left <= n) {
+            dfs(n, left + 1, right, item + '(', ans);
+        }
+        if (right < left) {
+            dfs(n, left, right + 1, item + ')', ans);
+        }
     }
 }
