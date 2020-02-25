70fce66d9feecc50aebdc16ac0320eabc4a00174
houjinxiang <houjinxiang@fangdd.com>
2020-02-25 09:39:52 +0800

    https://leetcode.com/problems/range-sum-query-mutable/

---
+++ src/main/java/com/hjx/leetcode/problem0307/NumArray.java
@@ -0,0 +1,16 @@
+package com.hjx.leetcode.problem0307;
+
+public class NumArray {
+
+    public NumArray(int[] nums) {
+
+    }
+
+    public void update(int i, int val) {
+
+    }
+
+    public int sumRange(int i, int j) {
+
+    }
+}
--- src/main/java/com/hjx/leetcode/problem0307/Solution.java
+++
@@ -1,5 +0,0 @@
-package com.hjx.leetcode.problem0307;
-
-public class Solution {
-    // TODO hjx 线段树
-}
