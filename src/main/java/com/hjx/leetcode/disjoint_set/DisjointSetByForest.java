a5107436d8961b8c3fc8278a6ff6abe3c831bb36
houjinxiang <houjinxiang@fangdd.com>
2020-03-02 15:05:30 +0800

    disjoint set

--- src/main/java/com/hjx/leetcode/disjoint_set/DisjointSetByForest.java
+++ src/main/java/com/hjx/leetcode/disjoint_set/DisjointSetByForest.java
@@ -1,5 +1,7 @@
 package com.hjx.leetcode.disjoint_set;
 
+import java.util.Arrays;
+
 /**
  * @author houjinxiang
  * 并查集
@@ -10,20 +12,22 @@ public class DisjointSetByForest {
 
     private int[] size;
 
-    private int count;
+    private Integer count;
 
     public DisjointSetByForest(int n) {
+        count = n;
         ids = new int[n];
         size = new int[n];
         for (int i = 0; i < n; i++) {
             ids[i] = i;
             size[i] = 1;
         }
-        count = n;
     }
 
-    public int find(int p) {
+    public int findSet(int p) {
         while (p != ids[p]) {
+            // 路径压缩
+            // p的父节点id[p]更新为id[p]的父节点ids[ids[p]]
             ids[p] = ids[ids[p]];
             p = ids[p];
         }
@@ -31,18 +35,45 @@ public class DisjointSetByForest {
     }
 
     public void union(int p, int q) {
-        int i = ids[p];
-        int j = ids[q];
+        int i = findSet(p);
+        int j = findSet(q);
         if (i == j) {
             return;
         }
         if (size[i] < size[j]) {
-            ids[i] = j;
+            // 规模小的向规模大的合并
             size[j] += size[i];
+            ids[i] = ids[j];
         } else {
-            ids[j] = i;
             size[i] += size[j];
+            ids[j] = ids[i];
         }
         count--;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int elementCount = 8;
+
+        DisjointSetByForest disjointSet = new DisjointSetByForest(elementCount);
+
+        disjointSet.union(0, 5);
+        // [5, 1, 2, 3, 4, 5, 6, 7]
+        System.out.println(Arrays.toString(disjointSet.ids));
+
+        System.out.println("------------------------------");
+        disjointSet.union(2, 5);
+        System.out.println(Arrays.toString(disjointSet.ids));
+        // [5, 1, 5, 3, 4, 5, 6, 7]
+        System.out.println("------------------------------");
+        disjointSet.union(2, 5);
+        System.out.println(Arrays.toString(disjointSet.ids));
+        // [5, 1, 5, 3, 4, 5, 6, 7]
+        System.out.println("------------------------------");
+        System.out.println(Arrays.toString(disjointSet.ids));
+        // [5, 1, 5, 5, 4, 5, 6, 7]
     }
 }
