05e9182da8846ab0eef45fd9f195088cff9d2ca3
houjinxiang <houjinxiang@fangdd.com>
2020-03-05 16:16:18 +0800

    https://leetcode.com/problems/linked-list-cycle/

--- src/main/java/com/hjx/leetcode/problem0141/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0141/Solution.java
@@ -4,7 +4,10 @@ import com.hjx.leetcode.entity.ListNode;
 
 public class Solution {
     /**
-     * 设置两个指针，一个快，一个慢，快的指针每次走两步，慢的指针每次走一步，如果快指针和慢指针相遇，则说明有环
+     * 快慢指针
+     * 设置两个指针，一个快，一个慢，快的指针每次走两步，慢的指针每次走一步，
+     * 如果快指针和慢指针相遇，则说明有环
+     *
      * @param head 链表头节点
      * @return
      */
@@ -12,11 +15,11 @@ public class Solution {
         if (head == null || head.next == null) {
             return false;
         }
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) {
+        ListNode fastNode = head, slowNode = head;
+        while (fastNode != null && fastNode.next != null) {
+            fastNode = fastNode.next.next;
+            slowNode = slowNode.next;
+            if (fastNode == slowNode) {
                 return true;
             }
         }
