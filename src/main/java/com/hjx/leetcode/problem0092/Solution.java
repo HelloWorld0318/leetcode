18fab18056aedf3342c10e7421d4a0c9b0174cdb
houjinxiang <houjinxiang@fangdd.com>
2020-03-05 12:51:41 +0800

    https://leetcode.com/problems/reverse-linked-list-ii/

--- src/main/java/com/hjx/leetcode/problem0092/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0092/Solution.java
@@ -5,31 +5,40 @@ import com.hjx.leetcode.entity.ListNode;
 public class Solution {
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        if (head == null || head.next == null || n - m == 0) {
+        if (head == null || head.next == null || m == n) {
             return head;
         }
 
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode pre = dummy;
-        ListNode cur = pre.next;
-        ListNode next = cur.next;
-        int changeLength = n - m + 1;
+        int changeLen = n - m + 1;
+        ListNode preHead = null;
+        ListNode result = head;
 
-        while (m-- > 1) {
-            pre = pre.next;
-            cur = cur.next;
-            next = next.next;
+        while (head != null && --m > 0) {
+            preHead = head;
+            head = head.next;
         }
 
-        while (changeLength-- > 1) {
-            cur.next = next.next;
-            next.next = pre.next;
-            pre.next = next;
+        ListNode mofifyListTail = head;
+        ListNode newHead = null;
 
-            next = cur.next;
+        while (head != null && changeLen > 0) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+
+            head = next;
+            changeLen--;
         }
-        return dummy.next;
+
+        mofifyListTail.next = head;
+
+        if (preHead != null) {
+            preHead.next = newHead;
+        } else {
+            result = newHead;
+        }
+
+        return result;
     }
 
 }
