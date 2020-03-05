4542d1162864c6de03f599f658492b89bc51ea46
houjinxiang <houjinxiang@fangdd.com>
2020-03-05 16:39:28 +0800

    problem0142

--- src/main/java/com/hjx/leetcode/problem0142/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0142/Solution.java
@@ -4,17 +4,21 @@ import com.hjx.leetcode.entity.ListNode;
 
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slowNode1 = head, quickNode = head;
-        while (quickNode != null && quickNode.next != null) {
-            slowNode1 = slowNode1.next;
-            quickNode = quickNode.next.next;
-            if (slowNode1 == quickNode) {
-                ListNode slowNode2 = head;
-                while (quickNode != slowNode2) {
-                    quickNode = quickNode.next;
-                    slowNode2 = slowNode2.next;
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                ListNode meetNode = fast;
+                while (head != meetNode) {
+                    head = head.next;
+                    meetNode = meetNode.next;
                 }
-                return quickNode;
+                return head;
             }
         }
         return null;
