ac1f971e7dc3eb0614d764acca55861a226dab4d
houjinxiang <houjinxiang@fangdd.com>
2020-03-08 09:49:17 +0800

    https://leetcode.com/problems/permutations-ii/

--- src/main/java/com/hjx/leetcode/problem0047/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0047/Solution.java
@@ -3,26 +3,25 @@ package com.hjx.leetcode.problem0047;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
+class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>(1 << nums.length);
-        if (nums.length > 0) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums != null && nums.length > 0) {
             dfs(nums, 0, ans);
         }
         return ans;
     }
 
-    private void dfs(int[] nums, int index, List<List<Integer>> ans) {
+    public void dfs(int[] nums, int index, List<List<Integer>> ans) {
         if (index == nums.length) {
-            List<Integer> oneResult = new ArrayList<>(nums.length);
+            List<Integer> oneAns = new ArrayList<>();
             for (int i = 0; i < nums.length; i++) {
-                oneResult.add(nums[i]);
+                oneAns.add(nums[i]);
             }
-            ans.add(oneResult);
-            return;
+            ans.add(oneAns);
         }
+
         for (int i = index; i < nums.length; i++) {
-            // 如果num[index]到num[i-1]当中有与num[i]相等的元素,那就不要交换元素了
             if (seen(nums, index, i)) {
                 continue;
             }
@@ -32,16 +31,16 @@ public class Solution {
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swap(int[] nums, int i, int j) {
+        nums[i] = (nums[i] + nums[j]) - (nums[j] = nums[i]);
     }
 
-    private boolean seen(int[] nums, int start, int end) {
-        for (int i = start; i < end; i++) {
-            if (nums[i] == nums[end]) {
-                return true;
+    public boolean seen(int[] nums, int start, int end) {
+        if (end > start) {
+            for (int i = start; i < end; i++) {
+                if (nums[i] == nums[end]) {
+                    return true;
+                }
             }
         }
         return false;
