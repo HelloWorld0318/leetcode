3df50073e840d985098b7fa7079f1e852833c21d
houjinxiang <houjinxiang@fangdd.com>
2020-03-08 22:39:22 +0800

    https://leetcode.com/problems/gray-code/

--- src/main/java/com/hjx/leetcode/problem0089/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0089/Solution.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    public List<Integer> grayCode(int n) {
+    public List<Integer> grayCodeByMathematicalFormula(int n) {
         List<Integer> ans = new ArrayList<>();
         if (n >= 0) {
             int size = 1 << n;
@@ -18,5 +18,18 @@ class Solution {
     public int binaryToGray(int n) {
         // 数学公示，整数n的Gray码是 n^(n/2)
         return n ^ (n >> 1);
+    }
+
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+        ans.add(0);
+        for (int i = 0; i < n; i++) {
+            int heightestBit = 1 << i;
+            int size = ans.size();
+            for (int j = size - 1; j >= 0; j--) {
+                ans.add(heightestBit | ans.get(j));
+            }
+        }
+        return ans;
     }
 }
