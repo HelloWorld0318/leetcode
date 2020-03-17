4d925d25d3894d6ea75127760b7359fecf4aa4b2
houjinxiang <houjinxiang@fangdd.com>
2020-03-17 14:54:06 +0800

    https://leetcode.com/problems/corporate-flight-bookings/

--- src/main/java/com/hjx/leetcode/problem1109/Solution.java
+++ src/main/java/com/hjx/leetcode/problem1109/Solution.java
@@ -1,9 +1,25 @@
 package com.hjx.leetcode.problem1109;
 
 public class Solution {
+
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        return brutalSolution(bookings, n);
+        int[] ans = new int[n];
+
+        for (int i = 0; i < bookings.length; i++) {
+            ans[bookings[i][0] - 1] += bookings[i][2];
+
+            if (bookings[i][1] < n) {
+                ans[bookings[i][1]] -= bookings[i][2];
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            ans[i] += ans[i - 1];
+        }
+
+        return ans;
     }
+
 
     public int[] brutalSolution(int[][] bookings, int n) {
         int[] ans = new int[n];
@@ -11,11 +27,12 @@ public class Solution {
             int[] item = bookings[i];
             int left = item[0] - 1;
             int right = item[1] - 1;
-            int bookingCount = item[2];
+            int seat = item[2];
             for (int j = left; j <= right; j++) {
-                ans[j] += bookingCount;
+                ans[j] += seat;
             }
         }
         return ans;
     }
+
 }
