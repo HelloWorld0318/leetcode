db5dd77c45b96aec10e50912b7c1bdb88b34ebe7
houjinxiang <1440538105@qq.com>
2023-04-11 09:23:07 +0800

    循环打印1到100

---
+++ src/main/java/com/hjx/leetcode/juc/MulThreadPrint1To100.java
@@ -0,0 +1,54 @@
+package com.hjx.leetcode.juc;
+
+public class MulThreadPrint1To100 {
+
+    private volatile int i = 0;
+    private volatile int flag = 0;
+
+    public static void main(String[] args) {
+        MulThreadPrint1To100 test = new MulThreadPrint1To100();
+        new Thread(test.new Thread1()).start();
+        new Thread(test.new Thread2()).start();
+        new Thread(test.new Thread3()).start();
+    }
+
+    class Thread1 implements Runnable {
+        @Override
+        public void run() {
+            while (i < 101) {
+                if (flag == 0) {
+                    System.out.println("Thread1-" + i);
+                    i++;
+                    flag = 1;
+                }
+            }
+        }
+    }
+
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (i < 101) {
+                if (flag == 1) {
+                    System.out.println("Thread2-" + i);
+                    i++;
+                    flag = 2;
+                }
+            }
+        }
+    }
+
+    class Thread3 implements Runnable {
+        @Override
+        public void run() {
+            while (i < 101) {
+                if (flag == 2) {
+                    System.out.println("Thread3-" + i);
+                    i++;
+                    flag = 0;
+                }
+            }
+        }
+    }
+}
+
