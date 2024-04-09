fad7a627a55cd359c2c1d98423304167c03cba1f
houjinxiang <1440538105@qq.com>
2024-04-09 11:50:14 +0800

    lru

---
+++ src/main/java/com/hjx/leetcode/problem0146/LRUCacheUserLinkedHashMap.java
@@ -0,0 +1,23 @@
+package com.hjx.leetcode.problem0146;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @param <K>
+ * @param <V>
+ * @author houjinxiang
+ */
+public class LRUCacheUserLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
+
+    private int capacity;
+
+    public LRUCacheUserLinkedHashMap(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > capacity;
+    }
+}
