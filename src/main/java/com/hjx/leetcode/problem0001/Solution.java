34cbddbde2693d4db589a8cf8df567fb8bfd0b72
houjinxiang <houjinxiang.hjx@digital-engine.com>
2024-12-26 22:53:37 +0800

    problem0001

--- src/main/java/com/hjx/leetcode/problem0001/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0001/Solution.java
@@ -3,16 +3,20 @@ package com.hjx.leetcode.problem0001;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author houjinxiang
+ */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        if (nums != null || nums.length > 0) {
-            Map<Integer, Integer> value2Index = new HashMap((int) (nums.length / 0.75));
-            for (int i = 0; i < nums.length; i++) {
-                if (value2Index.containsKey(target - nums[i])) {
-                    return new int[]{value2Index.get(target - nums[i]), i};
-                }
-                value2Index.put(nums[i], i);
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> value2Index = new HashMap((int) (nums.length / 0.75) + 1);
+        for (int i = 0; i < nums.length; i++) {
+            if (value2Index.containsKey(target - nums[i])) {
+                return new int[]{value2Index.get(target - nums[i]), i};
             }
+            value2Index.put(nums[i], i);
         }
         return null;
     }
