5332fc2752b16fa88a7cfb62a8240fb4dd0078d0
houjinxiang <houjinxiang.hjx@digital-engine.com>
2025-01-05 18:08:49 +0800

    two sum

--- src/main/java/com/hjx/leetcode/problem0015/Solution.java
+++ src/main/java/com/hjx/leetcode/problem0015/Solution.java
@@ -51,4 +51,54 @@ public class Solution {
         return ans;
     }
 
+    public List<List<Integer>> threeSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length - 2) {
+            int val = nums[i];
+            List<List<Integer>> twoSumResult = twoSum(nums, i + 1, nums.length - 1, target - val);
+            if (!twoSumResult.isEmpty()) {
+                for (List<Integer> integers : twoSumResult) {
+                    List<Integer> oneResult = new ArrayList<>();
+                    oneResult.add(val);
+                    oneResult.addAll(integers);
+                    res.add(oneResult);
+                }
+            }
+            while (i < nums.length && nums[i] == val) {
+                i++;
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> twoSum(int[] nums, int start, int end, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+            int leftVal = nums[start], rightVal = nums[end];
+            if (sum < target) {
+                while (start < end && nums[start] == leftVal) {
+                    start++;
+                }
+            } else if (sum > target) {
+                while (start < end && nums[end] == rightVal) {
+                    end--;
+                }
+            } else {
+                res.add(new ArrayList<>(Arrays.asList(leftVal, rightVal)));
+                while (start < end && nums[start] == leftVal) {
+                    start++;
+                }
+                while (start < end && nums[end] == rightVal) {
+                    end--;
+                }
+            }
+        }
+        return res;
+    }
 }
--- src/test/java/com/hjx/leetcode/problem0015/SolutionTest.java
+++ src/test/java/com/hjx/leetcode/problem0015/SolutionTest.java
@@ -16,4 +16,14 @@ public class SolutionTest {
         List<List<Integer>> res = solution.threeSum(nums);
         System.out.println(res);
     }
+
+
+    @Test
+    public void threeSum2() {
+        Solution solution = new Solution();
+        int[] nums = {-2, 0, 0, 2, 2};
+        List<List<Integer>> res = solution.threeSum(nums, 0);
+        System.out.println(res);
+    }
+
 }
