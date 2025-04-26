b262373d34505d9a140032944c485cfcdbfffe2f
houjinxiang <1440538105@qq.com>
2025-04-26 15:13:15 +0800

    add

--- src/main/java/com/hjx/leetcode/problem1206/Skiplist.java
+++ src/main/java/com/hjx/leetcode/problem1206/Skiplist.java
@@ -4,16 +4,16 @@ import java.util.Random;
 
 public class Skiplist {
     static final int MAX_LEVEL = 10;
-    private SkiplistNode dummy;
+    private SkipListNode dummy;
     private Random random;
 
     public Skiplist() {
-        this.dummy = new SkiplistNode(-1, MAX_LEVEL);
+        this.dummy = new SkipListNode(-1, MAX_LEVEL);
         this.random = new Random();
     }
 
-    public void find(int target, SkiplistNode[] ns) {
-        SkiplistNode cur = dummy;
+    public void find(int target, SkipListNode[] ns) {
+        SkipListNode cur = dummy;
         for (int i = MAX_LEVEL - 1; i >= 0; i--) {
             while (cur.next[i] != null && cur.next[i].val < target) {
                 cur = cur.next[i];
@@ -23,15 +23,15 @@ public class Skiplist {
     }
 
     public boolean search(int target) {
-        SkiplistNode[] ns = new SkiplistNode[MAX_LEVEL];
+        SkipListNode[] ns = new SkipListNode[MAX_LEVEL];
         find(target, ns);
         return ns[0].next[0] != null && ns[0].next[0].val == target;
     }
 
     public void add(int num) {
-        SkiplistNode[] ns = new SkiplistNode[MAX_LEVEL];
+        SkipListNode[] ns = new SkipListNode[MAX_LEVEL];
         find(num, ns);
-        SkiplistNode node = new SkiplistNode(num, MAX_LEVEL);
+        SkipListNode node = new SkipListNode(num, MAX_LEVEL);
         for (int i = 0; i < MAX_LEVEL; i++) {
             node.next[i] = ns[i].next[i];
             ns[i].next[i] = node;
@@ -42,9 +42,9 @@ public class Skiplist {
     }
 
     public boolean erase(int num) {
-        SkiplistNode[] ns = new SkiplistNode[MAX_LEVEL];
+        SkipListNode[] ns = new SkipListNode[MAX_LEVEL];
         find(num, ns);
-        SkiplistNode node = ns[0].next[0];
+        SkipListNode node = ns[0].next[0];
         if (node == null || node.val != num) {
             return false;
         }
@@ -56,7 +56,7 @@ public class Skiplist {
 
     public void print() {
         for (int i = MAX_LEVEL - 1; i >= 0; i--) {
-            SkiplistNode cur = dummy;
+            SkipListNode cur = dummy;
             while (cur != null) {
                 System.out.print(cur.val + "->");
                 cur = cur.next[i];
@@ -65,13 +65,13 @@ public class Skiplist {
         }
     }
 
-    private class SkiplistNode {
+    private class SkipListNode {
         int val;
-        SkiplistNode[] next;
+        SkipListNode[] next;
 
-        public SkiplistNode(int val, int maxLevel) {
+        public SkipListNode(int val, int maxLevel) {
             this.val = val;
-            this.next = new SkiplistNode[maxLevel];
+            this.next = new SkipListNode[maxLevel];
         }
     }
 }
