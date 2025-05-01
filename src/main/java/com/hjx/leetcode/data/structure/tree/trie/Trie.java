d7ef7dcc4e08afc5b27f4bc388a65d6b6f442840
houjinxiang <1440538105@qq.com>
2025-05-01 13:41:54 +0800

    字典树的实现

--- src/main/java/com/hjx/leetcode/data/structure/tree/trie/Trie.java
+++ src/main/java/com/hjx/leetcode/data/structure/tree/trie/Trie.java
@@ -1,5 +1,6 @@
 package com.hjx.leetcode.data.structure.tree.trie;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -99,6 +100,33 @@ public class Trie {
                 System.out.println();
                 preOrderTrie(entry.getValue(), layer + 1);
             }
+        }
+    }
+
+    private class TrieNode {
+
+        /**
+         * 当前节点的字符值
+         */
+        public char ch;
+
+        /**
+         * 当前节点的孩子节点，key是孩子节点值,value是孩子节点的下一个字典树
+         */
+        public Map<Character, TrieNode> children = new HashMap<>();
+
+        /**
+         * 是否是叶子节点
+         */
+        public boolean isLeaf;
+
+        public TrieNode(char ch) {
+            this.ch = ch;
+            isLeaf = false;
+        }
+
+        public TrieNode() {
+            isLeaf = false;
         }
     }
 }
